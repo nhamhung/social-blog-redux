48e94bbf859c411f56e68ef9bed6e1573468f5c6
snivJL <julien.lejay@gmail.com>
2021-01-29 11:03:25 +0700

    changed redirect to login

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -9,7 +9,7 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
         {...rest}
         render={(props) =>
           !isAuthenticated && !loading ? (
-            <Redirect to="/" />
+            <Redirect to="/login" />
           ) : (
             <Component {...props} />
           )
