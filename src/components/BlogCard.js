94b98e5b4e60b3735b03b891f117520bfab8b814
nhamhung <nhamhung.gttn@gmail.com>
2021-01-29 23:00:05 +0800

    blog-list-card basic implementation

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,18 +1,6 @@
 import React, { useState } from 'react';
 import BlogModal from './BlogModal';
-import {
-  MDBBtn,
-  MDBCard,
-  MDBCardBody,
-  MDBCardImage,
-  MDBCardTitle,
-  MDBCardText,
-  MDBView,
-  MDBMask,
-  MDBIcon,
-  MDBCol,
-  MDBRow,
-} from 'mdbreact';
+import { MDBBtn, MDBView, MDBMask, MDBIcon, MDBCol, MDBRow } from 'mdbreact';
 
 const BlogCard = ({ blog }) => {
   const [modal, setModal] = useState(false);
@@ -25,26 +13,6 @@ const BlogCard = ({ blog }) => {
   return (
     <div className='blogCard' onClick={handleToggle}>
       <BlogModal modal={modal} handleToggle={handleToggle} blogDetails={blog} />
-      {/* <MDBCol style={{ maxWidth: '22rem' }}> */}
-      {/* <MDBCard>
-          <MDBCardImage
-            className='img-fluid'
-            src={
-              blog.images &&
-              blog.images[0] &&
-              blog.images[0].match(img_url_regex)
-                ? blog.images[blog.images.length - 1]
-                : 'https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg'
-            }
-            waves
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Title: {blog.title}</MDBCardTitle>
-            <MDBCardText>Author: {blog.author.name}</MDBCardText>
-            <MDBBtn href='#'>See more</MDBBtn>
-            <MDBCardText>Last Updated: </MDBCardText>
-          </MDBCardBody>
-        </MDBCard> */}
       <MDBRow>
         <MDBCol lg='5'>
           <MDBView className='rounded z-depth-2 mb-lg-0 mb-4' hover waves>
@@ -74,12 +42,7 @@ const BlogCard = ({ blog }) => {
           <h3 className='font-weight-bold mb-3 p-0'>
             <strong>{blog.title}</strong>
           </h3>
-          <p>
-            Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-            impedit quo minus id quod maxime placeat facere possimus, omnis
-            voluptas assumenda est, omnis dolor repellendus et aut officiis
-            debitis.
-          </p>
+          <p>{blog.content}</p>
           <p>
             by
             <a href='#!'>
@@ -87,13 +50,16 @@ const BlogCard = ({ blog }) => {
             </a>
             , 19/08/2018
           </p>
-          <MDBBtn color='success' size='md' className='waves-light '>
+          <MDBBtn
+            color='success'
+            size='md'
+            className='waves-light '
+            onClick={handleToggle}
+          >
             Read more
           </MDBBtn>
         </MDBCol>
       </MDBRow>
-
-      {/* </MDBCol> */}
     </div>
   );
 };
