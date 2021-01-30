8e93fb2c523f87fb7728bd45886001a6fd8d3121
nngocjade <nngocjade@gmail.com>
2021-01-30 16:43:01 +0700

    added right side bar

--- src/components/FriendsButton.js
+++ src/components/FriendsButton.js
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import * as BsIcons from "react-icons/bs";
+import * as AiIcons from "react-icons/ai";
 import "../css/Buttons.css";
 
+import { Link } from "react-router-dom";
+import SearchUser from "./SearchUser";
+import { useDispatch, useSelector } from "react-redux";
+import FriendsActions from "../redux/actions/friends.actions";
+import Friend from "./Friend";
+
 const FriendsButton = () => {
+  // by default, sidebar is set to false meaning not displaying
+  const [sidebar, setSidebar] = useState(false);
+  const dispatch = useDispatch();
+  // Here ShowSidebar is like a toggle switch, from false(not open) to true (open)
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const friendsList = useSelector((state) => state.friends.friendList);
+  const loading = useSelector((state) => state.friends.loading);
+  console.log("friendList", friendsList);
+
+  useEffect(() => {
+    dispatch(FriendsActions.getFriendList());
+  }, [dispatch]);
+
   return (
     <div className="Friends-Button">
-      <div className="floating-button friends">
-        <BsIcons.BsPeople />
-      </div>
+      <Link to="#" className="floating-button friends">
+        <div className="floating-button friends" onClick={showSidebar}>
+          <BsIcons.BsPeople />
+        </div>
+      </Link>
+      {/* By default the right sidebar is hidden  */}
+      <nav
+        className={
+          sidebar ? "right-side-nav-menu active" : "right-side-nav-menu"
+        }
+      >
+        <ul className="right-side-nav-menu-items" onClick={showSidebar}>
+          <li className="right-side-navbar-toggle">
+            <Link to="#" className="right-side-menu-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          <li>
+            <SearchUser />
+          </li>
+          {/* fetch/map friends list */}
+          {!loading ? (
+            friendsList.map((friend, index) => (
+              <Friend friend={friend} key={index} />
+            ))
+          ) : (
+            <h1>loading</h1>
+          )}
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/pages/UserHomePage.js
+++ src/pages/UserHomePage.js
@@ -1,43 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import BlogsActions from '../redux/actions/blogs.actions';
-import BlogCardList from '../components/BlogCardList';
-import SearchBar from '../components/SearchBar';
-import Sort from '../components/Sort';
-import { MDBContainer, MDBRow } from 'mdbreact';
-import FriendsButton from '../components/FriendsButton';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import BlogsActions from "../redux/actions/blogs.actions";
+import BlogCardList from "../components/BlogCardList";
+import "../css/Navbar.css";
+import FriendsButton from "../components/FriendsButton";
 import WritePostButton from "../components/WritePostButton";
-import authActions from "../redux/actions/auth.actions";
-import '../css/Navbar.css';
 
 const UserHomePage = () => {
   const dispatch = useDispatch();
-  const [searchTerm, setSearchTerm] = useState('');
-  const [sortOrder, setSortOrder] = useState('newest');
-
   useEffect(() => {
     dispatch(BlogsActions.BlogsData());
-    dispatch(authActions.getUser());
   }, [dispatch]);
-
-  const handleSearch = (e) => {
-    setSearchTerm(e.target.value);
-  };
-
-  const handleSort = (order) => {
-    setSortOrder(order);
-  };
-
   return (
-    <MDBContainer>
-      <MDBRow className='searchBarContainer'>
-        <SearchBar handleSearch={handleSearch} />
-        <Sort handleSort={handleSort} />
-      </MDBRow>
-      <BlogCardList searchTerm={searchTerm} sortOrder={sortOrder} />
+    <div className="userHomePage">
+      <BlogCardList />
       <FriendsButton />
       <WritePostButton />
-    </MDBContainer>
+    </div>
   );
 };
 
