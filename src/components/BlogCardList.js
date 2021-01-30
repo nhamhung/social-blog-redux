b98716c463ba5ea3d13a4f3358925f511b521e5a
nhamhung <nhamhung.gttn@gmail.com>
2021-01-30 11:35:49 +0800

    implement:search and sort

--- src/components/BlogCardList.js
+++ src/components/BlogCardList.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogCard from './BlogCard';
-
 import { useSelector } from 'react-redux';
 
-const BlogCardList = () => {
+const BlogCardList = ({ searchTerm, sortOrder }) => {
+  const [filterBlogList, setFilterBlogList] = useState([]);
   const blogList = useSelector((state) => state.blogs.blogs);
   const loading = useSelector((state) => state.blogs.loading);
 
   console.log('blog list', blogList);
+  console.log(sortOrder);
+
+  useEffect(() => {
+    setFilterBlogList(
+      blogList.filter((x) =>
+        x.title.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+  }, [searchTerm]);
+
+  useEffect(() => {
+    if (sortOrder === 'newest') {
+      setFilterBlogList([
+        ...blogList.sort(
+          (x, y) => Date.parse(y.createdAt) - Date.parse(x.createdAt)
+        ),
+      ]);
+    } else {
+      setFilterBlogList([
+        ...blogList.sort(
+          (x, y) => Date.parse(x.createdAt) - Date.parse(y.createdAt)
+        ),
+      ]);
+    }
+  }, [sortOrder]);
+
+  console.log(searchTerm);
 
   return (
     <div className='blogCardList'>
       {!loading ? (
-        blogList.map((blog, index) => <BlogCard blog={blog} key={index} />)
+        filterBlogList.map((blog, index) => (
+          <BlogCard blog={blog} key={index} />
+        ))
       ) : (
         <h1>Loading</h1>
       )}
--- src/pages/UserHomePage.js
+++ src/pages/UserHomePage.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import BlogsActions from '../redux/actions/blogs.actions';
 import BlogCardList from '../components/BlogCardList';
+import SearchBar from '../components/SearchBar';
+import Sort from '../components/Sort';
+import { MDBRow } from 'mdbreact';
 import '../css/Navbar.css';
 
 const UserHomePage = () => {
   const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder, setSortOrder] = useState('newest');
+
   useEffect(() => {
     dispatch(BlogsActions.BlogsData());
   }, [dispatch]);
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSort = (order) => {
+    setSortOrder(order);
+  };
+
   return (
     <div className='userHomePage'>
-      <BlogCardList />
+      <MDBRow>
+        <SearchBar handleSearch={handleSearch} />
+        <Sort handleSort={handleSort} />
+      </MDBRow>
+      <BlogCardList searchTerm={searchTerm} sortOrder={sortOrder} />
     </div>
   );
 };
