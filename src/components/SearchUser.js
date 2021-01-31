2ebc38d6d8c63d01e47fdc93e27b699fa3d4e65d
nngocjade <nngocjade@gmail.com>
2021-02-01 01:39:07 +0700

    restyled rightSideBar, SearchUser & searchFriend inputs

--- src/components/SearchUser.js
+++ src/components/SearchUser.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import FriendsActions from "../redux/actions/friends.actions";
+import "../css/SearchInput.css";
+
 const SearchUser = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const history = useHistory();
@@ -16,14 +18,15 @@ const SearchUser = () => {
     console.log(searchTerm);
   }, [searchTerm]);
   return (
-    <>
+    <div className="search-input-container">
       <input
+        className="search-input user"
         onChange={(e) => setSearchTerm(e.target.value)}
         onKeyUp={(e) => handleSubmit(e)}
         type="text"
         placeholder="Search user..."
       />
-    </>
+    </div>
   );
 };
 
