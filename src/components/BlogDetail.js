ae1f2553883e691ec2f62eddb1607cab97db0c2e
nngocjade <nngocjade@gmail.com>
2021-02-01 09:52:30 +0700

    restyled some components in blog detail page

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   MDBContainer,
   MDBRow,
@@ -9,59 +9,62 @@ import {
   MDBView,
   MDBBtn,
   MDBIcon,
-} from 'mdbreact';
-import Moment from 'react-moment';
-import ReactionIcons from './ReactionIcons';
+} from "mdbreact";
+import Moment from "react-moment";
+import ReactionIcons from "./ReactionIcons";
 
 const BlogDetail = ({ blogDetails }) => {
   const img_url_regex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
 
   return (
-    <MDBCard className='my-5 px-5 pb-5 blogDetail'>
-      <MDBCardBody>
-        <MDBRow>
-          <MDBCol md='12'>
-            <MDBCard reverse>
-              <MDBView hover cascade waves>
-                <img
-                  src={
-                    blogDetails.images &&
-                    blogDetails.images[0] &&
-                    blogDetails.images[0].match(img_url_regex)
-                      ? blogDetails.images[blogDetails.images.length - 1]
-                      : 'https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg'
-                  }
-                  alt=''
-                  className='img-fluid'
-                />
-                <MDBMask overlay='white-slight' className='waves-light' />
-              </MDBView>
-              <MDBCardBody cascade className='text-center'>
-                <h2 className='font-weight-bold'>
-                  <a href='#!'>{blogDetails.title}</a>
-                </h2>
-                <p>
-                  Written by
-                  <a href='#!'>
-                    <strong> {blogDetails.author.name} - </strong>
-                  </a>
-                  <Moment fromNow>{blogDetails.createdAt}</Moment>
-                </p>
-                <ReactionIcons
-                  reactions={blogDetails.reactions}
-                  flag={'Blog'}
-                  id={blogDetails._id}
-                />
-                <MDBBtn>
-                  <MDBIcon far icon='comments' />
-                </MDBBtn>
-                <span className='counter'>{blogDetails.reviewCount}</span>
-              </MDBCardBody>
-            </MDBCard>
-            <MDBContainer className='mt-5'>{blogDetails.content}</MDBContainer>
-          </MDBCol>
-        </MDBRow>
-      </MDBCardBody>
+    <MDBCard className="my-5 px-5 pb-5 modal-blogDetail">
+      <MDBRow>
+        <MDBCol md="12">
+          <MDBCard reverse>
+            <MDBView hover cascade waves>
+              <img
+                src={
+                  blogDetails.images &&
+                  blogDetails.images[0] &&
+                  blogDetails.images[0].match(img_url_regex)
+                    ? blogDetails.images[blogDetails.images.length - 1]
+                    : "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+                }
+                alt=""
+                className="img-fluid"
+              />
+              <MDBMask overlay="white-slight" className="waves-light" />
+            </MDBView>
+            <MDBCardBody cascade className="text-center">
+              <h2 className="font-weight-bold">
+                <a href="#!">{blogDetails.title}</a>
+              </h2>
+              <p>
+                Written by
+                <a href="#!">
+                  <strong> {blogDetails.author.name} - </strong>
+                </a>
+                <Moment fromNow>{blogDetails.createdAt}</Moment>
+              </p>
+              <ReactionIcons
+                reactions={blogDetails.reactions}
+                flag={"Blog"}
+                id={blogDetails._id}
+              />
+              {/* -------COMMENT ICON AND COUNTER */}
+              <div className="comment-and-counter">
+                <div className="comment-icon-button">
+                  <MDBIcon far icon="comments" />
+                </div>
+                <span className="counter">{blogDetails.reviewCount}</span>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+          <MDBContainer className="mt-5">
+            Content: {blogDetails.content}
+          </MDBContainer>
+        </MDBCol>
+      </MDBRow>
     </MDBCard>
   );
 };
